1ecd81635e62f651f23d5ace5f6fefbb1afc51be
eli <qq1358722950@gmail.com>
2024-03-16 13:55:59 +0800

    docs: fix typos

--- org-count-words.el
+++ org-count-words.el
@@ -33,7 +33,7 @@ See `org-element-map' for details."
   :type '(list symbol))
 
 (defcustom org-count-words-no-recursive-elemnts '(drawer)
-  "List of element types that should be exlucded in recursive map.
+  "List of element types that should be exlucded in recursive mapping.
 
 See `org-element-map' for details."
   :group 'org-count-words
